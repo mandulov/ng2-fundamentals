3a1254e3adf6c9a68613aff576280314d7c54624
Kostadin Mandulov <mandulov@gmail.com>
2017-06-14 16:48:46 +0300

    seed events, ngIf

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -4,17 +4,21 @@ import { Component, Input } from '@angular/core'
     selector: 'event-thumbnail',
     template: `
       <div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
-        <div>
+        <h2>{{event?.name}}</h2>
+        <div>Date: {{event?.date}}</div>
+        <div>Time: {{event?.time}}</div>
+        <div>Price: \${{event?.price}}</div>
+        <div *ngIf="event?.location">
           <span>Location: {{event.location.address}}</span>
           <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+        </div>
+        <div *ngIf="event?.onlineUrl">
+          <span>Online URL: {{event.onlineUrl}}</span>
         </div>
       </div>
     `,
     styles: [`
+      .thumbnail { min-height: 210px; }
       .pad-left { margin-left: 10px; }
       .well div { color: #bbb; }
     `],
